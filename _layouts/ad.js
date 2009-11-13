f415ff4043fcc5cfbaed249e772c1157bbdb5ce8
Pencil Case Studios <hello@pencilcasestudios.com>
2009-11-13 16:31:00 +0200

    Added CSS class selectors to allow ads to be styled.

--- _layouts/ad.js
+++ _layouts/ad.js
@@ -1,7 +1,6 @@
-// Display content
-var content = '<a href="{{ page.target_link }}">';
-var content = content + '<img src="{{ page.advertiser_image }}" alt="{{ page.advertiser_image_text }}" title="{{ page.advertiser_image_text }}" /><br />\n';
-var content = content + '</a>\n';
-var content = content + '<p>{{ page.advertiser_body_copy }}</p>\n';
+// Display the content of the advertisement
+
+var content = '<a href="{{ page.target_link }}"><img class="grapevine_image" src="{{ page.advertiser_image }}" alt="{{ page.advertiser_image_text }}" title="{{ page.advertiser_image_text }}" border="0" height="90" /></a>\n';
+var content = content + '<p class="grapevine_text">{{ page.advertiser_body_copy }}</p>\n';
 
 document.write(content);
