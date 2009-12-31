ac4678a8f58f09159caf7bf1211e409554163619
Pencil Case Studios <hello@pencilcasestudios.com>
2009-12-31 14:51:18 +0200

    Added a Thank You page.

--- _layouts/ad.js
+++ _layouts/ad.js
@@ -1,5 +1,7 @@
 // Register this request for an advertisement 
 
+
+/*
 var xmlhttp;
 if (window.XMLHttpRequest) {
 	// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -12,7 +14,7 @@ else {
 
 xmlhttp.open("GET", "http://cgi.sfu.ca/~smaboshe/cgi-bin/register.php" , true);
 xmlhttp.send(null);
-
+*/
 
 // Display the content of the advertisement
 var content = '<a href="{{ page.target_link }}"><img class="grapevine_image" src="{{ page.advertiser_image }}" alt="{{ page.advertiser_image_text }}" title="{{ page.advertiser_image_text }}" border="0" height="90" /></a>\n';
